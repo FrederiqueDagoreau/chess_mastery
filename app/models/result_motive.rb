b620f156c1d9fa373345b59c232204aaacb29efb
Efflam Castel <efflamm.castel@gmail.com>
2024-03-26 19:12:40 +0000

    adding some result motives

--- app/models/result_motive.rb
+++ app/models/result_motive.rb
@@ -1,2 +1,3 @@
 class ResultMotive < ApplicationRecord
+  validates_uniqueness_of :name
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+result_motives = {
+  "LPDO": "Loose Pieces Drop Off",
+  "Zeitnot": "You have less than 5 increments when your opponent has more than 20",
+  "Perfect Game": "Opponent plays a perfect game",
+  "Blunder Check": "You put a piece en prise",
+  "Next Forcing Move": "You missed a direct forcing move from your opponent"
+}
+
+result_motives.each do |name, description|
+  ResultMotive.find_or_create_by!(name:, description:)
+end
